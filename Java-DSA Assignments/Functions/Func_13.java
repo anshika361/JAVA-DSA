0b06bfedf9c7870558a72b17a8917e724a495695
Anshika <anshikaagarwal631@gmail.com>
2022-01-20 17:39:36 +0530

    add files

---
+++ Java-DSA Assignments/Functions/Func_13.java
@@ -0,0 +1,16 @@
+package com.anshika;
+
+import java.util.Scanner;
+
+public class Func_13 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println("The prime numbers between numbers " + a + " and " + b + " are:");
+    }
+
+
+
+}
